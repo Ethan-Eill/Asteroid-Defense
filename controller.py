786b1ee5d4854a76943ba9d091ca86c906675c5a
Ethan Eill <97324170+Ethan-Eill@users.noreply.github.com>
2023-03-30 15:36:42 -0400

    Add files via upload

--- controller.py
+++ controller.py
@@ -1,18 +1,23 @@
 import pygame
 import model
-import view
+from view import main_menu_view
 
 #initiate game
 def init():
-    view.init()
+    global game_state
+    game_state = 'main_menu'
+    main_menu_view.init()
 
 #main game loop that controls all the input whilst playing the game
 def game_loop():
+    #game state is used to track which part of the game to display
+    #ie main menu, game, options
     while True:
-        #get events and loop through events
-        events = pygame.event.get()
-        for event in events:
-            #if player quits then exit game
-            if(event.type == pygame.QUIT):
-                pygame.quit()
-                quit()
+        #if the user is in the main menu
+        if game_state == 'main_menu':
+            main_menu_view.main_menu()
+        elif game_state == 'play':
+            print("in play game_state")
+        elif game_state == 'options':
+            print("in options game_state")
+        pygame.display.update()
